1659bd9ad86d9732ea66366e989fc305ccf9d0e4
Vivek G <audit536@gmail.com>
2023-12-05 20:39:00 +0530

    Update error message

--- extension-source-code/popup.js
+++ extension-source-code/popup.js
@@ -3,7 +3,7 @@ const copyButton = document.getElementById('copyButton');
 
 copyButton.addEventListener('click', function () {
     const messageToUI = document.querySelector("#message")
-    messageToUI.textContent = "Attempted to copy"
+    messageToUI.textContent = "Attempted to copy. If this did not work, reload the page and make sure the email is open"
 
     // Get the currently active tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
