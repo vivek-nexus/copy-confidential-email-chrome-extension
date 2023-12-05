eab3dc7e49eaea6d99aea47f2c0494019debbc9c
Vivek G <audit536@gmail.com>
2023-12-05 19:37:23 +0530

    Fix copy to clipboard not working

--- extension-source-code/content.js
+++ extension-source-code/content.js
@@ -1,4 +1,31 @@
 // content.js
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    alert("Is the confidential email open in the current tab?")
+    if (request.type === 'copyText') {
+        if (document.querySelector(".hi")) {
+            const element = document.querySelector(".hi").parentElement.children[2];
+            let textContentVariable = [];
+
+            if (element) {
+                logChildNodeText(element, textContentVariable);
+                const textContentString = textContentVariable.join('\n');
+
+                showEmailText(textContentString)
+
+                // navigator.clipboard.writeText(textContentString).then(function () {
+                //     alert("Text was successfully copied to clipboard!");
+                // }).catch(function () {
+                //     alert("Failed to copy to clipboard")
+                // });
+            }
+        }
+        else
+            alert("Looks the confidential email is not open in the current tab!")
+    }
+});
+
 function logChildNodeText(node, textContentVariable) {
     if (node.nodeType === Node.TEXT_NODE) {
         if (node.textContent.trim() !== '') {
@@ -12,23 +39,62 @@ function logChildNodeText(node, textContentVariable) {
     }
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    alert("Is the confidential email open in the current tab?")
-    if (request.type === 'copyText') {
-        if (document.querySelector(".hi")) {
-            const element = document.querySelector(".hi").parentElement.children[2];
-            let textContentVariable = [];
+function showEmailText(text) {
+    // Banner CSS
+    let html = document.querySelector("html");
+    let obj = document.createElement("div");
+    let textarea = document.createElement("textarea");
+    let copyButton = document.createElement("button");
+    let closeButton = document.createElement("button");
+    obj.style.cssText = modalCSS;
+    textarea.style.cssText = textAreaCSS
+    textarea.value = text;
+    copyButton.textContent = "Copy"
+    closeButton.textContent = "Close"
 
-            if (element) {
-                logChildNodeText(element, textContentVariable);
-                const textContentString = textContentVariable.join('\n');
+    obj.append(textarea);
+    obj.append(copyButton);
+    obj.append(closeButton)
 
-                navigator.clipboard.writeText(textContentString).then(function () {
-                    alert("Text was successfully copied to clipboard!");
-                });
-            }
-        }
-        else
-            alert("Looks the confidential email is not open in the current tab!")
-    }
-});
+    if (html)
+        html.append(obj);
+
+    copyButton.addEventListener("click", () => {
+        navigator.clipboard.writeText(text).then(function () {
+            copyButton.textContent = "Copied"
+            setTimeout(() => {
+                copyButton.textContent = "Copy"
+            }, 3000);
+        }).catch(function () {
+            alert("Failed to copy to clipboard")
+        });
+    })
+
+    closeButton.addEventListener("click", () => {
+        obj.remove()
+    })
+}
+
+
+const modalCSS = `background: rgb(255 255 255 / 75%); 
+    backdrop-filter: blur(16px); 
+    position: fixed;
+    top: 10%; 
+    left: 0; 
+    right: 0; 
+    margin-left: auto; 
+    margin-right: auto;
+    max-width: 780px;
+    height: 75dvh;  
+    z-index: 1000; 
+    padding: 0rem 1rem;
+    border-radius: 8px; 
+    font-size: 1rem; 
+    line-height: 1.5; 
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;`;
+
+const textAreaCSS = `width: 100%;
+    height: 90%;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+`;
